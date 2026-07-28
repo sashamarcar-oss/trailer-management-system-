4a2d44446fa89090c243f35ea77f58a7adeaa3b3
KEFFA <muthurikeffa254@gmail.com>
2026-07-28 16:21:54 +0300

    Add card description to card UI component

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -33,4 +33,16 @@ const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTML
 );
 CardTitle.displayName = "CardTitle";
 
-export { Card, CardHeader, CardContent, CardTitle };
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn('text-sm text-muted-foreground', className)}
+    {...props}
+  />
+));
+CardDescription.displayName = 'CardDescription';
+
+export { Card, CardHeader, CardContent, CardTitle, CardDescription };
